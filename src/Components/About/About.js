6377e0ecfc66ab26b5a234031393d153f26c0e82
MDRahul47e <rahulahames@gmail.comm>
2022-04-05 14:23:49 +0600

    error-solve

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,11 +8,10 @@ const About = () => {
             <h1>About Our Service</h1>
             <p>Top Gear Auto Service Bd Ltd is a full-service auto center in Dhaka specializing in repairs and preventative maintenance for all models of Luxurious Cars.
 
-                If you’re looking for an expert, reliable auto shop to trust your vehicle to then we are sure you will love us. We offer general automotive services, including regular maintenance, repairs, and more. Our experts have the knowledge to work with all makes and models, including Toyota, Nissan, Audi, Mercedes, BMW, and many more.
+                If youre looking for an expert, reliable auto shop to trust your vehicle to then we are sure you will love us. We offer general automotive services, including regular maintenance, repairs, and more. Our experts have the knowledge to work with all makes and models, including Toyota, Nissan, Audi, Mercedes, BMW, and many more.
 
-                At Top Gear, with the help of our latest technology and tools, we assess your vehicle and provide the best service required for your car.  We guarantee all of our work, so we know you’ll be happy with the end result and will be at peace of mind with your beloved car!</p>
-            At Top Gear, with the help of our latest technology and tools, we assess your vehicle and provide the best service required for your car.  We guarantee all of our work, so we know you’ll be happy with the end result and will be at peace of mind with your beloved car!</p>
-        </div >
+                At Top Gear, with the help of our latest technology and tools, we assess your vehicle and provide the best service required for your car.  We guarantee all of our work, so we know youll be happy with the end result and will be at peace of mind with your beloved car!</p>
+        </div>
     );
 };
 
